e1cef50f80c9b08ca62b11bb55716670ab99a057
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-19 19:36:53 +1000

    showing error message properly

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,28 +2,30 @@
  * Created by Rakesh on 19/07/15.
  */
 
-app.controller('MainCtrl',['$scope','forecastService', function ($scope,forecastService) {
+app.controller('MainCtrl', ['$scope', 'forecastService', function ($scope, forecastService) {
 
     $scope.data = null;
 
     $scope.getData = function () {
-        forecastService.getForecast($scope.cityName).then(onComplete,onError);
+        forecastService.getForecast($scope.cityName).then(onComplete, onError);
     };
 
-    var onComplete = function(data){
-        if(data.hasOwnProperty('message')){
-            if(data.message.toString().indexOf("Error") >= 0){
-                $scope.data = "City not found";
+    var onComplete = function (data) {
+        if (data.hasOwnProperty('message')) {
+            if (data.message.toString().indexOf("Error") >= 0) {
+                $scope.error = true;
+                $scope.data = "City not found for \"" + $scope.cityName + "\"";
                 return;
             }
         }
 
-
+        $scope.error = false;
         $scope.data = data;
 
     };
 
-    var onError = function(){
+    var onError = function () {
+        $scope.error = true;
         $scope.data = "Network Error"
     };
 
